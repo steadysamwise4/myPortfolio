f1d50fb3310b54c8f560772c9f788a68205b4b1b
Sam Davenport <samueldavenport@att.net>
2022-02-12 11:24:47 -0600

    project info filled in and iterated over using .map method in projectList component. gitHub link added to Modal component, still need to style it.

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 function Modal({currentProject, onClose}) {
-    const { name, description, index} = currentProject;
+    const { name, description, index, owner} = currentProject;
 
     return (
       <div className="modalBackdrop">
         <div className="modalContainer">
-          <h3 className="modalTitle">{name}</h3>
-          <img alt={name} src={require(`../../assets/images/${index}.jpg`)} />
+          <h2 className="modalTitle">
+              {name}
+              <a href={`https://github.com/${owner}/${name}`}>
+                  Github
+              </a>
+          </h2>
+          <img alt={name} src={require(`../../assets/images/${index}.png`)} />
           <p>{description}</p>
           <button type="button" onClick={onClose}>Close this modal</button>
         </div>
--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -6,48 +6,43 @@ function ProjectList() {
 
   const [projects] = useState([
     {
-      name: "trippin worldwide",
+      name: "trippin-worldwide",
       description:
         "This application will provide a user the ability to journalize information about their travels such as description, start and end dates, city and country, activities, and cuisine as well as provide an opportunity to review/comment on other users' adventures. In addition, the most visited cities will be displayed via a map to help plan any future journeys. The web application uses Node.js and Express.js to create a RESTful API, Handlebars.js as a templating engine, MySQL and Sequelize ORM for the database, dotenv and bcrypt for securing sensitive data, bootstrap/CSS for styling, AWS S3 Bucket for storage, and Google Maps Geocoding and JavaScript APIs for mapping locations visited within a map view.",
+        owner: "hkapk",
     },
-    { name: "", description: "" },
-    { name: "", description: "" },
-    { name: "", description: "" },
-    { name: "", description: "" },
-    { name: "", description: "" },
+    { name: "deep-thoughts", description: "Boot camp led MERN stack social media style app. Also using GraphQL API.", owner: "steadysamwise4", },
+    { name: "photo-port", description: "Boot camp led project done as a hands-on introduction to React.JS", owner: "steadysamwise4", },
+    { name: "budget-even-offline", description: "Boot camp challenge to enhance a budget app, so that if functions offline and as a PWA. Served by MongoDB and uploaded to Heroku.", owner: "steadysamwise4", },
+    { name: "brilliant-tech", description: "Keep up with recent trends in programming by reading fellow developers' blog posts and linked articles. Create your own posts and link your favorite articles as well.", owner: "steadysamwise4", },
+    { name: "explore_national_parks", description: "Explore National Parks is a Web Application that allows the user to view the details of all National Parks in America and plan their potential visit. The Application uses National Park Service API, OpenWeather API and is written in HTML, CSS and Javascript.", owner: "ramantv", },
   ]);
   const [currentProject, setCurrentProject] = useState(projects[0]);
     
-      
-
-      
-
       const toggleModal = (image, i) => {
         setCurrentProject({...image, index: i});
         setIsModalOpen(!isModalOpen);
+        console.log(image, i)
       }
 
   return (
-    <div>
+    <div className='my-2'>
       {isModalOpen && <Modal currentProject={currentProject}
                              onClose={toggleModal}/>}
-       {/* <div className="flex-row">
-           {currentProject.map((image, i) => (
+       <div className="flex-row my-2 mx-2">
+           {projects.map((image, i) => (
+               <div className='col-12 col-lg-5 mx-2'>
+               <h5>{image.name}</h5>
                <img 
-                src={require(`../../assets/images/${i}.jpg`)}
+                src={require(`../../assets/images/${i}.png`)}
                 alt={image.name}
                 className="img-thumbnail mx-1"
                 onClick={() => toggleModal(image, i)}
                 key={image.name}
                />
+               </div>
            ))}
-       </div> */}
-       <img src={require(`../../assets/images/${0}.png`)}
-       alt={currentProject.name}
-       onClick={() => toggleModal(currentProject, 0)}
-       className="img-thumbnail mx-1"
-       key={currentProject.name}
-       />
+       </div>
     </div>
   )
 }
